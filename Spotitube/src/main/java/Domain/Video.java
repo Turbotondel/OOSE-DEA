8525832c5b8a55c740154523fbd1c758e69abd16
nymtesx <you@example.com>
2017-04-04 11:42:02 +0200

    niggu

---
+++ Spotitube/src/main/java/Domain/Video.java
@@ -0,0 +1,7 @@
+package Domain;
+
+/**
+ * Created by nymtes on 4-4-2017.
+ */
+public class Video {
+}
---
+++ Spotitube/src/main/java/Service/PlaylistService.java
@@ -0,0 +1,7 @@
+package Service;
+
+/**
+ * Created by nymtes on 4-4-2017.
+ */
+public class PlaylistService {
+}
