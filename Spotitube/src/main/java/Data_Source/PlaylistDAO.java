3fc90c4baec7c46f0eae419191b4adcaab2acea0
nymtesx <you@example.com>
2017-04-04 12:22:58 +0200

    new classes + functions

---
+++ Spotitube/src/main/java/Data_Source/PlaylistDAO.java
@@ -0,0 +1,28 @@
+package Data_Source;
+
+import Domain.Playlist;
+
+import java.util.List;
+
+/**
+ * Created by nymtes on 4-4-2017.
+ */
+public class PlaylistDAO {
+
+
+    public List list(){
+        return null;
+    };
+
+    public List findByOwner() {
+        return null;
+    }
+
+    public void save(Playlist playlist){
+
+    }
+
+    public void delete(int playListId){
+    }
+
+}
