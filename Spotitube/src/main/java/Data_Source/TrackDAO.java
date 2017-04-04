4c5bdc7c04be311ca451c3ebeedf8c25165e4dca
nymtesx <you@example.com>
2017-04-04 12:22:08 +0200

    new classes + functions

---
+++ Spotitube/src/main/java/Data_Source/TrackDAO.java
@@ -0,0 +1,20 @@
+package Data_Source;
+
+import java.util.List;
+
+/**
+ * Created by nymtes on 4-4-2017.
+ */
+public class TrackDAO {
+
+
+    public List list(){
+        return null;
+    }
+
+    public List findByTitle(){
+        return null;
+    }
+    
+
+}
