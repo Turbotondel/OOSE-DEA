5e3d357cee8e690c70c054fbc97c26c99fc325ff
tim de kruijf <tim de kruijf>
2017-04-06 10:01:17 +0200

    Added trackservice

---
+++ Spotitube/src/main/java/Service/TrackService.java
@@ -0,0 +1,12 @@
+package Service;
+
+import Domain.Playlist;
+
+import java.util.List;
+
+/**
+ * Created by timde on 6-4-2017.
+ */
+public interface TrackService {
+    List<Playlist> insertvideo(String name)
+}
