96c7960954ee964f9c50f639794d096da120c6d5
tim de kruijf <tim de kruijf>
2017-04-06 11:43:00 +0200

    More modifications

--- Spotitube/src/main/java/Service/TrackService.java
+++ Spotitube/src/main/java/Service/TrackService.java
@@ -8,5 +8,5 @@ import java.util.List;
  * Created by timde on 6-4-2017.
  */
 public interface TrackService {
-    List<Playlist> insertvideo(String name)
+    List<Playlist> insertvideo(String name);
 }
---
+++ Spotitube/src/main/java/Service/local/LocalTrackService.java
@@ -0,0 +1,7 @@
+package Service.local;
+
+/**
+ * Created by timde on 6-4-2017.
+ */
+public class LocalTrackService {
+}
